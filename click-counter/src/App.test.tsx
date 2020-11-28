846911de3089b38c2fb468957aeb95dde8413e6b
Yuri <yuriflores2012@gmail.com>
2020-11-28 11:48:36 -0300

    adding increment button and counter display test

--- click-counter/src/App.test.tsx
+++ click-counter/src/App.test.tsx
@@ -11,10 +11,26 @@ test('render without error', () => {
   expect(appComponent.length).toBe(1);
 });
 
-test('renders button', () => {});
+test('renders button', () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
 
-test('render counter display', () => {});
+test('render counter display', () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
 
-test('counter starts at 0', () => {});
+test('counter starts at 0', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
 
-test('clicking on button increments counter display', () => {});
+test('clicking on button increments counter display', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
