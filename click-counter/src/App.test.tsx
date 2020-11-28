734c027d75c67238825b54e364e5a50a2f941ea1
Yuri <yuriflores2012@gmail.com>
2020-11-28 01:14:23 -0300

    initial files

---
+++ click-counter/src/App.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('render without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders button', () => {});
+
+test('render counter display', () => {});
+
+test('counter starts at 0', () => {});
+
+test('clicking on button increments counter display', () => {});
