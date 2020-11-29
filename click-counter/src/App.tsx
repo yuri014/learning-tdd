4569772defebae6fa589bd7c5c2edc201b10d85c
Yuri <yuriflores2012@gmail.com>
2020-11-29 12:12:29 -0300

    test challenge - counter display does not go below 0

--- click-counter/src/App.tsx
+++ click-counter/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <button data-test='increment-button' onClick={() => setCount(count + 1)}>
         Increment counter
       </button>
-      <button data-test='decrement-button' onClick={() => setCount(count - 1)}>
+      <button data-test='decrement-button' onClick={() => count > 0 && setCount(count - 1)}>
         Decrement counter
       </button>
     </div>
