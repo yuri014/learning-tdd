566798571834cc9fbabf9ce04c86b751de4439b2
Yuri <yuriflores2012@gmail.com>
2020-11-29 15:41:39 -0300

    adding state to prevent error message in the initial render

--- click-counter/src/App.tsx
+++ click-counter/src/App.tsx
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   return (
     <div data-test='component-app'>
       <h1 data-test='counter-display'>
         The counter is currently&nbsp;<span data-test='count'>{count}</span>
       </h1>
+      <p className={errorMessage ? 'error' : 'hidden'} data-test='error-message'>
+        The counter can't go below zero!
+      </p>
       <button data-test='increment-button' onClick={() => setCount(count + 1)}>
         Increment counter
       </button>
-      <button data-test='decrement-button' onClick={() => count > 0 && setCount(count - 1)}>
+      <button
+        data-test='decrement-button'
+        onClick={() => (count > 0 && setCount(count - 1))}
+      >
         Decrement counter
       </button>
     </div>
