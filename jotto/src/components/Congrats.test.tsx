ecce517dbd86faab73325f0605d1f934be9747f3
Yuri <yuriflores2012@gmail.com>
2020-12-06 20:23:34 -0300

    testing congrats component

---
+++ jotto/src/components/Congrats.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import { findByTestAttribute } from '../tests/testUtils';
+import Congrats from './Congrats';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => shallow(<Congrats {...props} />);
+
+test('renders without an error', () => {
+  const wrapper = setup();
+  const component = findByTestAttribute(wrapper, 'congrats-component');
+  expect(component.length).toBe(1);
+});
+
+test('renders no text when `success` prop is false', () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttribute(wrapper, 'congrats-component');
+  expect(component.text()).toBe('');
+});
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true });
+  const component = findByTestAttribute(wrapper, 'congrats-message');
+  expect(component.text().length).not.toBe(0);
+});
