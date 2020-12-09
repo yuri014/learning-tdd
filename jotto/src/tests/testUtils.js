214d8832f865eacd25db41d127d0b4f5424bd851
Yuri <yuriflores2012@gmail.com>
2020-12-09 10:59:43 -0300

    check props test

--- jotto/src/tests/testUtils.js
+++ jotto/src/tests/testUtils.js
@@ -1,2 +1,14 @@
+import checkPropTypes from 'check-prop-types';
+
 export const findByTestAttribute = (wrapper, value) =>
   wrapper.find(`[data-test="${value}"]`);
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
+};
