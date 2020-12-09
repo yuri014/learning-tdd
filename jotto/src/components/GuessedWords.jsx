b3d24df947f035ff03f3182ec1f5ebfd3ddb89e4
Yuri <yuriflores2012@gmail.com>
2020-12-09 15:45:44 -0300

    basic component implementation

--- jotto/src/components/GuessedWords.jsx
+++ jotto/src/components/GuessedWords.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuessedWords = props => <div></div>;
+const GuessedWords = props => {
+  const contents = () => {
+    if (props.guessedWords.length === 0) {
+      return (
+        <span data-test="guessed-instructions">
+          Try to guess the secret word!
+        </span>
+      );
+    }
+  };
+
+  return <div data-test="component-guessed-words">{contents()}</div>;
+};
 
 GuessedWords.propTypes = {
   guessedWords: PropTypes.arrayOf(
