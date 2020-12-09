385b2c7430d4e0c8760ffa468090cf1bde79af94
Yuri <yuriflores2012@gmail.com>
2020-12-09 18:48:16 -0300

    tailwindcss config

--- jotto/gatsby-config.js
+++ jotto/gatsby-config.js
@@ -5,5 +5,12 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('tailwindcss')],
+      },
+    },
+  ],
 };
