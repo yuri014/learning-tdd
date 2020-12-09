33c706c8f2ae92b6c483025278b3c5413864b465
Yuri <yuriflores2012@gmail.com>
2020-12-09 16:45:00 -0300

    implementing component if guessed words lenght are not 0

--- jotto/src/components/GuessedWords.jsx
+++ jotto/src/components/GuessedWords.jsx
@@ -10,6 +10,27 @@ const GuessedWords = props => {
         </span>
       );
     }
+    return (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letter</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.guessedWords.map((word, index) => (
+              <tr data-test="guessed-word" key={index}>
+                <th>{word.guessedWord}</th>
+                <th>{word.letterMatchCount}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   };
 
   return <div data-test="component-guessed-words">{contents()}</div>;
