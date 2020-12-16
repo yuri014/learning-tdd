d88b5663d61906c1bace7e58e0f5f2170e20329b
yuri014 <yuriflores2012@gmail.com>
2020-12-16 17:00:10 -0300

    input component implementation

--- jotto/src/components/Input.jsx
+++ jotto/src/components/Input.jsx
@@ -3,16 +3,29 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
-    return (
-      <div>
-        <input type="text" />
-      </div>
+    const content = this.props.success ? null : (
+      <form className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
+        <input
+          type="text"
+          className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+          data-test="input-box"
+          placeholder="enter guess word..."
+        />
+        <button
+          type="submit"
+          data-test="submit-button"
+          className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
+        >
+          Submit
+        </button>
+      </form>
     );
+    return <div data-test="component-input">{content}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return {};
+const mapStateToProps = ({ sucess }) => {
+  return { sucess };
 };
 
 export default connect(mapStateToProps)(Input);
