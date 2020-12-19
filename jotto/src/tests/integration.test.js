0f57d2e5499ffc3d28d12982cb8ca2f91a0d4a89
yuri014 <yuriflores2012@gmail.com>
2020-12-19 00:32:17 -0300

    test if updates state correctly for unsuccess guess

--- jotto/src/tests/integration.test.js
+++ jotto/src/tests/integration.test.js
@@ -2,8 +2,26 @@ import { storeFactory } from './testUtils';
 import { guessWord } from '../actions';
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuess = 'train';
   describe('no guessed words', () => {
-    test('updates state correctly for unsuccess guess', () => {});
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+    test('updates state correctly for unsuccess guess', () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState;
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          { guessedWords: unsuccessfulGuess, letterMatchCount: 3 },
+        ],
+      };
+      expect(newState).toEqual(expectedState);
+    });
     test('updates state correctly for success guess', () => {});
   });
   describe('some guessed words', () => {
