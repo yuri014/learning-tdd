3e616b8a687b4a32e34e578769bb8c130bc37014
yuri014 <yuriflores2012@gmail.com>
2020-12-19 00:22:40 -0300

    set up guess words integration test

---
+++ jotto/src/tests/integration.test.js
@@ -0,0 +1,13 @@
+import { storeFactory } from './testUtils';
+import { guessWord } from '../actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccess guess', () => {});
+    test('updates state correctly for success guess', () => {});
+  });
+  describe('some guessed words', () => {
+    test('updates state correctly for unsuccess guess', () => {});
+    test('updates state correctly for success guess', () => {});
+  });
+});
