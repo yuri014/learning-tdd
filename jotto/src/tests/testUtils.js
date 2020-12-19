93beba87bd04f0e196fb066c0ccc8a692e1880cf
yuri014 <yuriflores2012@gmail.com>
2020-12-19 00:01:10 -0300

    adding redux thunk and applying middlewares

--- jotto/src/tests/testUtils.js
+++ jotto/src/tests/testUtils.js
@@ -1,10 +1,14 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { middlewares } from '../configureStore';
+
 
 import rootReducer from '../reducers/index';
 
-export const storeFactory = initialState =>
-  createStore(rootReducer, initialState);
+export const storeFactory = initialState => {
+  const createStoreWithReducer = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithReducer(rootReducer, initialState);
+}
 
 export const findByTestAttribute = (wrapper, value) =>
   wrapper.find(`[data-test="${value}"]`);
